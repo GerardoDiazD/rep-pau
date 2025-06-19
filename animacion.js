ba43f59baa1bba9591e1162d11cfa34435fe9da0
Gerardo Díaz <gdiazd2000@hotmail.com>
2025-06-19 14:52:29 +0200

    animación

---
+++ animacion.js
@@ -0,0 +1,8 @@
+
+function iniciarAnimacion() {
+    setTimeout(() => {
+      document.getElementById("espera").style.display = "none";
+      document.getElementById("mensaje-final").style.display = "block";
+    }, 6000); // cambia a 8000 para 8 segundos de espera
+  }
+  
